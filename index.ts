7f7b6b7418dc627e4d0579c6b72fad78061f39ec
saad abid <msaadabid2326@gmail.comm>
2024-05-28 13:45:52 -0700

    push simple calculator

---
+++ index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+
+const answer = await inquirer.prompt([
+    { message: "Enter Your First Number", type: "number", name: "firstNumber" },
+    { message: "Enter Your second Number", type: "number", name: "secondNumber" },
+    {
+        message: "select one of operators to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["multiplicaion", "addition", "subtraction", "division"]
+    }
+]);
+
+// CONDITIONAL STATEMENT
+
+if (answer.operator === "addition"){
+    console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "subtraction"){
+    console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "multiplication"){
+    console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operator === "division"){
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {"please select write operator"};
